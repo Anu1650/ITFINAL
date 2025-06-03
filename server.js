78a5215749e22d94b0f5d8f7231663fdb51b65e3
Anu1650 <aniketigade@gmail.com>
2025-06-03 07:47:37 +0530

    your message

--- server.js
+++ server.js
@@ -9,15 +9,77 @@ app.set('views', path.join(__dirname, 'views'));
 // Serve static assets
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Pages list – all lowercase file names
-const pages = ['homepage', 'aboutus', 'mid-brain', 'blogs', 'contact', 'counseling', 'dbit', 'f', 'g', 'nlp', 'memory-techniques', 'personality', 'step', 'kmep', 'ycep', 'w1', 'w2', 'w3'];
+// Define routes explicitly
+app.get('/', (req, res) => {
+  res.render('homepage');
+});
 
-// Dynamic routes
-pages.forEach(page => {
-  const routePath = page === 'homepage' ? '/' : `/${page}`;
-  app.get(routePath, (req, res) => {
-    res.render(page);
-  });
+app.get('/aboutus', (req, res) => {
+  res.render('aboutus');
+});
+
+app.get('/mid-brain', (req, res) => {
+  res.render('mid-brain');
+});
+
+app.get('/blogs', (req, res) => {
+  res.render('blogs');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/counseling', (req, res) => {
+  res.render('counseling');
+});
+
+app.get('/dbit', (req, res) => {
+  res.render('dbit');
+});
+
+app.get('/f', (req, res) => {
+  res.render('f');
+});
+
+app.get('/g', (req, res) => {
+  res.render('g');
+});
+
+app.get('/nlp', (req, res) => {
+  res.render('nlp');
+});
+
+app.get('/memory-techniques', (req, res) => {
+  res.render('memory-techniques');
+});
+
+app.get('/personality', (req, res) => {
+  res.render('personality');
+});
+
+app.get('/step', (req, res) => {
+  res.render('step');
+});
+
+app.get('/kmep', (req, res) => {
+  res.render('kmep');
+});
+
+app.get('/ycep', (req, res) => {
+  res.render('ycep');
+});
+
+app.get('/w1', (req, res) => {
+  res.render('w1');
+});
+
+app.get('/w2', (req, res) => {
+  res.render('w2');
+});
+
+app.get('/w3', (req, res) => {
+  res.render('w3');
 });
 
 // 404 fallback
